f964f31e9f11a3031919b853be219c1184be31b3
Keefer Taylor <keefer@keefertaylor.com>
2020-08-14 16:20:54 -0700

    bump to 1.3.4

--- src/cli.ts
+++ src/cli.ts
@@ -18,7 +18,7 @@ import {
 } from '@tacoinfra/harbinger-lib'
 import * as commander from 'commander'
 
-const version = '1.3.3'
+const version = '1.3.4'
 
 const defaultTestnetNode = 'https://rpctest.tzbeta.net'
 const defaultMainnetNode = 'https://rpc.tzbeta.net'
