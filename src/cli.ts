885d29097fcc425c068a55b10b46d78f9034a3e7
Keefer Taylor <keefer@keefertaylor.com>
2020-08-24 18:22:02 -0700

    cut 1.7.0

--- src/cli.ts
+++ src/cli.ts
@@ -18,7 +18,7 @@ import {
 } from '@tacoinfra/harbinger-lib'
 import * as commander from 'commander'
 
-const version = '1.6.0'
+const version = '1.7.0'
 
 const defaultTestnetNode = 'https://rpctest.tzbeta.net'
 const defaultMainnetNode = 'https://rpc.tzbeta.net'
